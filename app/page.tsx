888b2118c14d94e4dbbe49d1ab99f55822866b95
elvisanthonyy <info.elvisanthony@gmail.com>
2025-11-09 15:34:59 +0100

    check session

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { getSession } from "@/libs/session";
+//import { getSession } from "@/libs/session";
 //import { redirect } from "next/navigation";
 /*import Nav from "./component/nav";
 import MainHome from "./component/MainHome";
@@ -6,10 +6,6 @@ import NavigationButtons from "./component/NavigationButtons";*/
 
 export default async function Home() {
   try {
-    const session = await getSession();
-    console.log("SESSION", session);
-    if (!session) return <div>No session</div>;
-
     return (
       <>
         <div>hello</div>
