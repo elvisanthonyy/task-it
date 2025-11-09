6f60a478402ac2968b6c176ff3934f40b088446b
elvisanthonyy <info.elvisanthony@gmail.com>
2025-11-09 15:40:50 +0100

    get sesioon directly

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
-//import { getSession } from "@/libs/session";
+import { getSession } from "@/libs/session";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 //import { redirect } from "next/navigation";
 /*import Nav from "./component/nav";
 import MainHome from "./component/MainHome";
@@ -6,6 +8,10 @@ import NavigationButtons from "./component/NavigationButtons";*/
 
 export default async function Home() {
   try {
+    const session = await getServerSession(authOptions);
+    console.log("SESSION", session);
+    if (!session) return <div>No sesion</div>;
+
     return (
       <>
         <div>hello</div>
