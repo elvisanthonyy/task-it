a49835e8a752d44d815faa2132e12a86536cb1ce
elvisanthonyy <info.elvisanthony@gmail.com>
2025-11-09 16:37:04 +0100

    external

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,13 @@
-export const runtime = "nodejs";
+import { getSession } from "@/libs/session";
 
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
-import dbConnect from "@/libs/dbConnection";
 //import { redirect } from "next/navigation";
 /*import Nav from "./component/nav";
 import MainHome from "./component/MainHome";
 import NavigationButtons from "./component/NavigationButtons";*/
 
 export default async function Home() {
-  await dbConnect();
   try {
-    const session = await getServerSession(authOptions);
+    const session = await getSession();
     console.log("SESSION", session);
     if (!session) return <div>No sesion</div>;
 
--- libs/session.ts
+++ libs/session.ts
@@ -1,6 +1,10 @@
+export const runtime = "nodejs";
+
+import dbConnect from "./dbConnection";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
 
 export async function getSession() {
+  await dbConnect();
   return await getServerSession(authOptions);
 }
