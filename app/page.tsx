1448dfb708ace5e9e15adef69db4e30e6f9df7a6
elvisanthonyy <info.elvisanthony@gmail.com>
2025-11-09 16:43:49 +0100

    nan, main and nav btns

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,21 @@
 import { getSession } from "@/libs/session";
 
-//import { redirect } from "next/navigation";
-/*import Nav from "./component/nav";
+import { redirect } from "next/navigation";
+import Nav from "./component/nav";
 import MainHome from "./component/MainHome";
-import NavigationButtons from "./component/NavigationButtons";*/
+import NavigationButtons from "./component/NavigationButtons";
 
 export default async function Home() {
   try {
     const session = await getSession();
-    console.log("SESSION", session);
-    if (!session) return <div>No sesion</div>;
+
+    if (!session) return redirect("/login");
 
     return (
       <>
-        <div>{session?.user?.name}</div>
+        <Nav />
+        <MainHome />
+        <NavigationButtons pageName="home" />
       </>
     );
   } catch (err) {
