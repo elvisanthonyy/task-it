7e3f68d6cb61ac476b11396d393b7e4236bd9f8b
elvisanthonyy <info.elvisanthony@gmail.com>
2025-11-09 15:24:44 +0100

    just getSesion

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,17 @@
-//import { getSession } from "@/libs/session";
+import { getSession } from "@/libs/session";
 //import { redirect } from "next/navigation";
-import Nav from "./component/nav";
+/*import Nav from "./component/nav";
 import MainHome from "./component/MainHome";
-import NavigationButtons from "./component/NavigationButtons";
+import NavigationButtons from "./component/NavigationButtons";*/
 
 export default async function Home() {
   try {
-    //const session = await getSession();
-    //console.log("SESSION", session);
+    const session = await getSession();
+    console.log("SESSION", session);
 
     return (
       <>
-        <div>
-          <Nav />
-          <MainHome />
-          <NavigationButtons pageName="home" />
-        </div>
+        <div>hello</div>
       </>
     );
   } catch (err) {
